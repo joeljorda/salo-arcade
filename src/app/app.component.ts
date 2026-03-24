70d2e1a457762f9bcfaaebbbe7877d36ccdb971c
Joel Jordà <joeljorda@tuta.io>
2026-03-24 23:34:41 +0100

    exercicis 3 i 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,43 @@ import { BarraCercaComponent } from './components/barra-cerca/barra-cerca.compon
 import { Element } from './models/element.model';
 import { ELEMENTS } from './mocks/dades-mock';
 import { LlistaElementsComponent } from './components/llista-elements/llista-elements.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BarraCercaComponent, LlistaElementsComponent],
+  imports: [BarraCercaComponent, LlistaElementsComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 
 export class AppComponent {
   elements: Element[] = ELEMENTS;
-  
+  searchMessage: string = '';
+
   onSearch(searchText: string): void {
-    const text = searchText.toLowerCase();
+    const text = searchText.trim().toLowerCase();
+
+    if (text.length === 0) {
+      this.elements = ELEMENTS;
+      this.searchMessage = '';
+      return;
+    }
+
+    if (text.length < 3) {
+      this.elements = [];
+      this.searchMessage = "Escriu mínim 3 lletres.";
+      return;
+    }
 
     this.elements = ELEMENTS.filter(element =>
       element.name.toLowerCase().includes(text)
     );
-  }  
+
+    if (this.elements.length === 0) {
+      this.searchMessage = "No s'ha trobat cap joc.";
+    } else {
+      this.searchMessage = '';
+    }
+  }
 }
--- src/app/components/barra-cerca/barra-cerca.component.ts
+++ src/app/components/barra-cerca/barra-cerca.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-barra-cerca',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './barra-cerca.component.html',
   styleUrl: './barra-cerca.component.scss'
 })
@@ -14,7 +15,7 @@ export class BarraCercaComponent {
   @Output() searchEvent = new EventEmitter<string>();
 
   onSearch(): void { 
-    this.searchEvent.emit(this.searchText);
+      this.searchEvent.emit(this.searchText);
   }
 
   onReset(): void{
