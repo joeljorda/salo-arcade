160b91b7301f08780e03559d34dfe58fe503d08c
Joel Jordà <joeljorda@tuta.io>
2026-05-10 16:13:36 +0200

    Exercici 3: Lazy Loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { DetailComponent } from './pages/detail/detail.component';
-import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { authGuard } from './guards/auth.guard';
@@ -11,9 +10,16 @@ import { authGuard } from './guards/auth.guard';
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'search', component: SearchComponent},
+  { path: 'search', component: SearchComponent },
   { path: 'detail/:id', component: DetailComponent },
-  { path: 'favorites', component: FavoritesComponent, canActivate: [authGuard] },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./pages/favorites/favorites.component').then(
+        (m) => m.FavoritesComponent,
+      ),
+    canActivate: [authGuard],
+  },
   { path: 'login', component: LoginComponent },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: 'home' },
