04d095fdac77c4e8e58747f281e689dc8f9b5743
Joel Jordà <joeljorda@tuta.io>
2026-05-10 11:49:21 +0200

    Crear components base

---
+++ src/app/components/element-card/element-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './element-card.component.html',
+  styleUrl: './element-card.component.scss'
+})
+export class ElementCardComponent {
+
+}
